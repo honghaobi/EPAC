045f34abbc52f8c568b0378f46fb181f6cd92ed5
henrybi <henrybi@u.washington.edu>
2016-03-09 15:17:39 -0800

    main function

--- js/app.js
+++ js/app.js
@@ -54,4 +54,26 @@
   });
 
 
+
+  //site render:
+
+  function init(){
+      console.log("working");
+      product1 = $('.product1').ThreeSixty({
+          totalFrames: 10, // Total no. of image you have for 360 slider
+          endFrame: 10, // end frame for the auto spin animation
+          currentFrame: 1, // This the start frame for auto spin
+          imgList: '.threesixty_images', // selector for image list
+          progress: '.spinner', // selector to show the loading progress
+          imagePath:'img/site', // path of the image assets
+          ext: '.png', // extention for the assets
+          height: 900,
+          width: 1440,
+          navigation: true,
+          disableSpin: true // Default false
+      });
+  }
+
+  init();
+
 })();
